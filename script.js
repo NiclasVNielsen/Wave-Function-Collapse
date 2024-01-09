ff8db1ce305c40cdb06c33fccc7d18d217214206
NiclasVNielsen <Niclasvnielsen@gmail.com>
2024-01-09 12:21:16 +0100

    Select random tile when available options are tied

--- script.js
+++ script.js
@@ -40,6 +40,25 @@ function orderArray(a, b){
     return a.order - b.order    
 }
 
+function shuffle(array) {
+    //* https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+    let currentIndex = array.length,  randomIndex;
+  
+    // While there remain elements to shuffle.
+    while (currentIndex > 0) {
+        // Pick a remaining element.
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+    
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+}
+
+
 let order = []
 const calcOrder = () => {
     order = []
@@ -47,6 +66,8 @@ const calcOrder = () => {
         if(tile.tiles.length > 1)
             order.push({index: index, order: tile.tiles.length})
     })
+    
+    shuffle(order)
     order.sort(orderArray)
 
     if(order.length > 0){
@@ -84,9 +105,6 @@ const createTile = (tile, x, y) => {
 
 const randomNumber = (max, min = 0) => {
     let random = Math.random()
-    if(random == 1){
-        random = 0;
-    }
     random = Math.floor(random * (max + 1))
     return random + min
 }
